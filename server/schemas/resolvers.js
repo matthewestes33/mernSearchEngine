ad65cd262a8e57254dd14f5cf048518d8892edd1
Matt Estes <me6384@gmail.com>
2022-08-03 12:33:42 -0700

    fill schemas/index and convert controllers/user-controller to schemas/resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,68 @@
+//import Apollo
+const { AuthenticationError } = require('apollo-server-express');
+// import user model
+const { User } = require('../models');
+// import sign token function from auth
+const { signToken } = require('../utils/auth');
+
+//see week 21, lesson 25-26 for guide
+const resolvers = {
+    Query: {
+        // get a single user by either their id or their username ??
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+                return userData;
+            }
+            throw new AuthenticationError('Please login.');
+        },
+    },
+
+    Mutation: {
+        // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Username not found. Please create an account.');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Password incorrect. Please try again.');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
+        saveBook: async (parent, { newBook }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: newBook } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('Please login.');
+        },
+        // remove a book from `savedBooks`
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('Please login.');
+        },
+    },
+};
+
+module.exports = resolvers;
